7ddf8fa27e68a3547cb88a7c78085b492cf83e15
Jon Fuller <jcfuller@sep.com>
2019-05-13 11:56:23 -0400

    breaking the service hoster out into a runner class that can be consumed by an API

--- ServiceHoster/Controller/Program.cs
+++ ServiceHoster/Controller/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.ServiceModel;
 using System.Threading;
 using CommandLine;
 
@@ -14,117 +10,36 @@ namespace ServiceHoster.Controller
         public static void Main(string[] args)
         {
             Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(opts => PidFile(opts, Run))
+                .WithParsed(Run)
                 .WithNotParsed(_ => { });
         }
 
         private static void Run(Options options)
         {
-            Console.Title = "WCF Service Hoster";
+            var runner = new ServiceRunner(options, Console.Out, Console.Error);
+            runner.Start(new AutoResetEvent(false));
 
-            var hosts = options.ServiceDlls
-                .Select(dll => new{dll, config = $"{dll}.config"}) // assumes the assembly's config file is in the same location, named the same with .config appended
-                .Where(h => File.Exists(h.dll) && File.Exists(h.config))
-                .Select(h =>
-                {
-                    Console.Out.WriteLine($"Creating ServiceHost for for {h.dll} ({h.config}).");
-
-                    return new AppDomainHost(h.dll, h.config);
-                })
-                .ToList();
-
-            foreach (var host in hosts)
-            {
-                Console.Out.WriteLine($"Opening services for ...{host.HostInfo.AssemblyPath}");
-                Console.Out.WriteLine($"  config: {host.HostInfo.AssemblyConfig}");
-                Console.Out.WriteLine($"  assembly version: {host.HostInfo.AssemblyVersion}");
-                Console.Out.WriteLine($"  file version: {host.HostInfo.FileVersion}");
-                Console.Out.WriteLine("  services: {0}",
-                    string.Join(", ", host.Services.Select(h => $"{h.Name}").ToArray()));
-                host.OpenServices();
-            }
-
-            Console.Out.WriteLine("Service Endpoints:");
-            hosts.SelectMany(h => h.Services).ToList().ForEach(service =>
-            {
-                Console.Out.WriteLine(service.Name);
-                Console.WriteLine($"  WSDL: {service.WsdlAddress ?? "N/A"}");
-                foreach (var endpoint in service.MexEndpoints)
-                    Console.WriteLine($"  {endpoint}");
-                foreach (var endpoint in service.Endpoints)
-                    Console.WriteLine($"  {endpoint}");
-            });
-
-            var closing = false;
             Console.CancelKeyPress += (sender, eventArgs) =>
             {
-                Console.Out.WriteLine($"Closing services...");
-                foreach (var host in hosts)
-                    host.CloseServices();
-                Console.Out.WriteLine($"Closed services...");
-                closing = true;
+                runner.Stop();
                 eventArgs.Cancel = true;
             };
             Console.Out.WriteLine("Press <Ctrl+C> to stop.");
-
-            while (!closing)
-            {
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-                if (options.HasStatus)
-                {
-                    var allOpen = hosts.SelectMany(h => h.Status).All(s => s.State == CommunicationState.Opened);
-
-                    UpdateStatus(
-                        options.StatusFile,
-                        allOpen ? "OK" : string.Join(", ", hosts.SelectMany(h => h.Status).Select(s => $"{s.ServiceName} - {s.State}")));
-                }
-            }
         }
+    }
 
-        private static void UpdateStatus(string filename, string status)
-        {
-            File.WriteAllText(filename, status);
-        }
+    public class Options
+    {
+        [Option('p', "pid-file", HelpText = "Location of the PID file. No PID file if not specified.")]
+        public string PidFile { get; set; }
 
-        private static void PidFile(Options options, Action<Options> runner)
-        {
-            try
-            {
-                if (options.HasPidFile)
-                {
-                    File.WriteAllText(options.PidFile, Process.GetCurrentProcess().Id.ToString());
-                }
+        [Option('s', "status-file", HelpText = "Location of the STATUS file. No STATUS file if not specified. Contains 'OK' in the nominal case.")]
+        public string StatusFile { get; set; }
 
-                runner(options);
-            }
-            finally
-            {
-                if (options.HasPidFile && File.Exists(options.PidFile))
-                {
-                    File.Delete(options.PidFile);
-                }
-            }
-        }
+        [Value(0, Min = 1, MetaName = "service dll's", HelpText = "List of service DLL's to host.")]
+        public IEnumerable<string> ServiceDlls { get; set; }
 
-        private class Options
-        {
-            [Option('p', "pid-file", HelpText = "Location of the PID file. No PID file if not specified.")]
-            public string PidFile { get; set; }
-
-            [Option('s', "status-file", HelpText = "Location of the STATUS file. No STATUS file if not specified. Contains 'OK' in the nominal case.")]
-            public string StatusFile { get; set; }
-
-            [Value(0, Min = 1, MetaName = "service dll's", HelpText = "List of service DLL's to host.")]
-            public IEnumerable<string> ServiceDlls { get; set; }
-
-            public bool HasPidFile
-            {
-                get { return PidFile != null; }
-            }
-            public bool HasStatus
-            {
-                get { return StatusFile != null; }
-            }
-        }
+        public bool HasPidFile => PidFile != null;
+        public bool HasStatus => StatusFile != null;
     }
 }
---
+++ ServiceHoster/Controller/ServiceRunner.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading;
+
+namespace ServiceHoster.Controller
+{
+    public class ServiceRunner : IDisposable
+    {
+        private readonly Options _options;
+        private readonly TextWriter _out;
+        private readonly TextWriter _err;
+        private readonly List<AppDomainHost> _hosts;
+        private bool _closing;
+
+        public ServiceRunner(Options options, TextWriter @out, TextWriter err)
+        {
+            _options = options;
+            _out = @out;
+            _err = err;
+
+            _hosts = _options
+                .ServiceDlls
+                .Select(dll => new
+                {
+                    dll,
+                    config = $"{dll}.config"
+                }) // assumes the assembly's config file is in the same location, named the same with .config appended
+                .Where(h => File.Exists(h.dll) && File.Exists(h.config))
+                .Select(h =>
+                {
+                    _out.WriteLine($"Creating ServiceHost for for {h.dll} ({h.config}).");
+
+                    return new AppDomainHost(h.dll, h.config);
+                })
+                .ToList();
+        }
+
+        public void Dispose()
+        {
+            Stop();
+        }
+
+        public void Stop()
+        {
+            _out.WriteLine($"Closing services...");
+            foreach (var host in _hosts.AsEnumerable().Reverse())
+            {
+                host.CloseServices();
+                _out.WriteLine($"  Closed {host.HostInfo.AssemblyPath}");
+            }
+            _out.WriteLine($"Closed services...");
+            _closing = true;
+        }
+
+        public void Start(AutoResetEvent started)
+        {
+            PidFile(() => RunServices(started));
+        }
+
+        private void PidFile(Action runner)
+        {
+            try
+            {
+                if (_options.HasPidFile)
+                {
+                    File.WriteAllText(_options.PidFile, Process.GetCurrentProcess().Id.ToString());
+                }
+
+                runner();
+            }
+            finally
+            {
+                if (_options.HasPidFile && File.Exists(_options.PidFile))
+                {
+                    File.Delete(_options.PidFile);
+                }
+            }
+        }
+
+        private void RunServices(AutoResetEvent onServicesStarted)
+        {
+            foreach (var host in _hosts)
+            {
+                _out.WriteLine($"Opening services for ...{host.HostInfo.AssemblyPath}");
+                _out.WriteLine($"  config: {host.HostInfo.AssemblyConfig}");
+                _out.WriteLine($"  assembly version: {host.HostInfo.AssemblyVersion}");
+                _out.WriteLine($"  file version: {host.HostInfo.FileVersion}");
+                _out.WriteLine("  services: {0}",
+                    string.Join(", ", host.Services.Select(h => $"{h.Name}").ToArray()));
+                host.OpenServices();
+            }
+
+            _out.WriteLine("Service Endpoints:");
+            _hosts.SelectMany(h => h.Services).ToList().ForEach(service =>
+            {
+                _out.WriteLine(service.Name);
+                _out.WriteLine($"  WSDL: {service.WsdlAddress ?? "N/A"}");
+                foreach (var endpoint in service.MexEndpoints)
+                    _out.WriteLine($"  {endpoint}");
+                foreach (var endpoint in service.Endpoints)
+                    _out.WriteLine($"  {endpoint}");
+            });
+            onServicesStarted.Set();
+
+            _closing = false;
+            while (!_closing)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                if (_options.HasStatus)
+                {
+                    var allOpen = _hosts.SelectMany(h => h.Status).All(s => s.State == CommunicationState.Opened);
+
+                    UpdateStatus(
+                        _options.StatusFile,
+                        allOpen
+                            ? "OK"
+                            : string.Join(", ",
+                                _hosts.SelectMany(h => h.Status).Select(s => $"{s.ServiceName} - {s.State}")));
+                }
+            }
+        }
+
+        private static void UpdateStatus(string filename, string status)
+        {
+            File.WriteAllText(filename, status);
+        }
+    }
+}
